962220a83ce96936b8efe5f108bbd1e0ba854028
Utkarsh Mahajan <utkarshrm568@gmail.com>
2021-07-20 23:02:40 +0530

    Basic Styling

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app =express();
 const path = require('path')
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate')
 const Campground =require('./models/campground')
 const methodOverride = require('method-override')
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -11,6 +12,7 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', {
 });
 app.use(methodOverride('_method'))
 app.use(express.urlencoded({extended:true}))
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'views'))
 
--- seeds/index.js
+++ seeds/index.js
@@ -21,9 +21,13 @@ const seedDB = async function(){
 
     for(let i=0;i<50;++i){
         let random1k = Math.floor(Math.random()*1000)
+        const price =  Math.floor(Math.random()*5000) +2000;
         const camp =new Campground({
             location: `${cities[random1k].city}, ${cities[random1k].state}`,
-            title:`${sample(descriptors)} ${sample(places)}`
+            title:`${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt quod cupiditate ratione repudiandae itaque voluptatum hic nemo quo voluptatibus. Perferendis illo atque incidunt. Vel totam tenetur facere! Ex, cumque sae",
+            price
         })
         await camp.save();
     }
